08bc310763e9952fff3608146ab1641361705e52
chenyixing <435021572@qq.com>
2017-12-22 10:57:57 +0800

    添加cocoapods以及WCDB，Masonry库

---
+++ XDCardTime/XDCardTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XDCardTime
+//
+//  Created by chenyixing on 2017/12/22.
+//  Copyright © 2017年 fiberhome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
